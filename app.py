9c29ddc6a501f7bba2bc7a419704dacbe51b6772
subbu2107 <subbramaniyan2107@gmail.com>
2025-07-27 23:08:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -410,6 +410,11 @@ def logout():
     session.clear()
     return redirect('/')
 
-if __name__ == '__main__':
+@app.before_first_request
+def setup():
     init_db()
+
+if __name__ == '__main__':
     app.run(debug=True)
+
+
